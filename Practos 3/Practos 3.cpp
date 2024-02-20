e95af38e4399dab598c4eac245d93322684a5dab
Anashechev <you@example.com>
2024-02-20 23:19:39 +0300

    -_-

---
+++ Practos 3/Practos 3.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+int main() {
+    setlocale(0, "");
+    const int rows = 3;
+    const int cols = 5;
+    double arr[rows][cols];
+
+    for (int i = 0; i < rows; ++i) {
+        std::cout << "Введите элементы для строки " << i + 1 << " (5 элементов): ";
+        for (int j = 0; j < cols; ++j) {
+            std::cin >> arr[i][j];
+        }
+    }
+
+    for (int i = 0; i < rows; ++i) {
+        double sum = 0.0;
+        for (int j = 0; j < cols; ++j) {
+            sum += arr[i][j];
+        }
+        double average = sum / cols;
+        std::cout << "Среднее арифметическое элементов в строке " << i + 1 << ": " << average << std::endl;
+    }
+
+    return 0;
+}
